6f163dd5e7641ef7d23c43a626275ddd35291ad2
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-10-28 18:47:39 +0000

    Test

--- db-api/src/main/java/net/sky/service/order/OrderService.java
+++ db-api/src/main/java/net/sky/service/order/OrderService.java
@@ -1,8 +1,13 @@
 package net.sky.service.order;
 
 
+import net.sky.service.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    @Autowired(required = false)
+    ProductService productService;
 }
