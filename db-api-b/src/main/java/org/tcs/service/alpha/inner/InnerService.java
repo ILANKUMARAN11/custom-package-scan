2a39f9f982bc50a2651a92df242f6787b1040b83
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-10-29 18:55:32 +0000

    Inner package Explicit scan

---
+++ db-api-b/src/main/java/org/tcs/service/alpha/inner/InnerService.java
@@ -0,0 +1,7 @@
+package org.tcs.service.alpha.inner;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InnerService {
+}
---
+++ db-api-b/src/test/java/ilan/service/InnerServiceTest.java
@@ -0,0 +1,33 @@
+package ilan.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.WHEN_AVAILABLE)
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
+@Slf4j
+@ActiveProfiles("inner")
+public class InnerServiceTest {
+    private final ApplicationContext applicationContext;
+
+    @Test
+    void beanCount() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        Long beanCount = Arrays.stream(allBeanNames)
+                .filter(bean -> applicationContext.getType(bean).getPackageName().startsWith("org.tcs.service"))
+                .peek(bean -> {
+                    log.info(">>>> BeanType:: {}, Bean Name :: {}" , applicationContext.getType(bean) , bean);
+                }).count();
+
+        assertEquals(1, beanCount);
+    }
+}
